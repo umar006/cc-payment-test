e7170bcc103ecafee811676183e7625bbfbaaa0e
umar006 <umaralfaruq597@gmail.com>
2024-05-21 20:07:22 +0700

    feat: deposit and withdraw form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>deposit</h2>
+      <form>
+        <label>
+          <input type="number" />
+        </label>
+        <button type="submit">deposit</button>
+      </form>
+      <h2>withdraw</h2>
+      <form>
+        <label>
+          <input type="number" />
+        </label>
+        <button type="submit">withdraw</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
