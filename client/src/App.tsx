63652c1b8fbf6cc6199bd5355564580f0d2f236d
umar006 <umaralfaruq597@gmail.com>
2024-05-21 20:19:53 +0700

    feat: handle change deposit and withdraw value

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,33 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [deposit, setDeposit] = useState('0');
+  const [withdraw, setWithdraw] = useState('0');
+
   return (
     <>
       <h2>deposit</h2>
       <form>
         <label>
-          <input type="number" />
+          <input
+            type="number"
+            onChange={(e) => setDeposit(e.target.value)}
+            min="0"
+            step="any"
+          />
         </label>
         <button type="submit">deposit</button>
       </form>
       <h2>withdraw</h2>
       <form>
         <label>
-          <input type="number" />
+          <input
+            type="number"
+            onChange={(e) => setWithdraw(e.target.value)}
+            min="0"
+            step="any"
+          />
         </label>
         <button type="submit">withdraw</button>
       </form>
