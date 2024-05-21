f03f2e8e5f8f5b86f88a18c3483548aee21b5315
umar006 <umaralfaruq597@gmail.com>
2024-05-21 16:26:37 +0700

    feat: apply trx to deposit api with highest isolation

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,11 @@
 import {
-  Inject,
-  Injectable,
-  UnprocessableEntityException,
+    Inject,
+    Injectable,
+    UnprocessableEntityException,
 } from '@nestjs/common';
+import { eq, sql } from 'drizzle-orm';
 import { DRIZZLE_PROVIDER, type DrizzlePostgres } from './drizzle.provider';
 import { users } from './user.schema';
-import { eq, sql } from 'drizzle-orm';
 
 @Injectable()
 export class AppService {
@@ -26,21 +26,25 @@ export class AppService {
       body: JSON.stringify(depositDto),
     }).catch((e) => e);
 
-    const [user] = await this.db
+    await this.db.transaction(async (tx) => {
+      const [user] = await tx
       .select()
       .from(users)
       .where(eq(users.name, fullName));
 
-    if (!user) {
-      await this.db.insert(users).values({
-        name: 'Umar Abdul Aziz Al-Faruq',
-        balance: depositDto.amount,
-      });
-    } else {
-      await this.db
+      if (!user) {
+        await tx.insert(users).values({
+          name: 'Umar Abdul Aziz Al-Faruq',
+          balance: depositDto.amount,
+        });
+      } else {
+        await tx
         .update(users)
         .set({ balance: sql`${users.balance} + ${depositDto.amount}` });
-    }
+      }
+    }, {
+      isolationLevel: 'serializable',
+    });
 
     return {
       order_id: depositDto.order_id,
