ef2b0f93ec6b20e320beb3fce14ca88622eafa35
umar006 <umaralfaruq597@gmail.com>
2024-05-21 12:58:15 +0700

    feat: deposit service

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,23 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  async deposit(depositDto: Record<string, any>) {
+    await fetch('https://youdomain.com/deposit', {
+      signal: AbortSignal.timeout(1000),
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${btoa('Umar Abdul Aziz Al-Faruq')}`,
+      },
+      body: JSON.stringify(depositDto),
+    }).catch((e) => e);
+
+    return {
+      order_id: depositDto.order_id,
+      amount: depositDto.amount,
+      status: 1,
+    };
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
