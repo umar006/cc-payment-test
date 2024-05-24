0759215b59b5484ace85368085daa0cf67525005
umar006 <umaralfaruq597@gmail.com>
2024-05-24 10:27:31 +0700

    feat: interface for app controller

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -2,35 +2,40 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { DepositDTO } from './dtos/deposit.dto';
 import { WithdrawDTO } from './dtos/withdraw.dto';
+import { AppTransactionController } from './interfaces/app-transaction-controller.interface';
 import { TransactionHistory } from './schemas/transaction-history.schema';
 import { User } from './schemas/user.schema';
 import { DepositResponse } from './types/deposit-response.type';
 import { WithdrawResponse } from './types/withdraw-response.type';
 
 @Controller()
-export class AppController {
+export class AppController implements AppTransactionController {
   constructor(private readonly appService: AppService) {}
 
   @Post('deposit')
-  async deposit(@Body() depositDto: DepositDTO): Promise<DepositResponse> {
+  async createDeposit(
+    @Body() depositDto: DepositDTO,
+  ): Promise<DepositResponse> {
     const response = await this.appService.createDeposit(depositDto);
     return response;
   }
 
   @Post('withdraw')
-  async withdraw(@Body() withdrawDto: WithdrawDTO): Promise<WithdrawResponse> {
+  async createWithdraw(
+    @Body() withdrawDto: WithdrawDTO,
+  ): Promise<WithdrawResponse> {
     const response = await this.appService.createWithdraw(withdrawDto);
     return response;
   }
 
   @Get('balance')
-  async balance(): Promise<User> {
+  async getBalance(): Promise<User> {
     const response = await this.appService.getBalance();
     return response;
   }
 
   @Get('histories')
-  async transactionHistories(): Promise<TransactionHistory[]> {
+  async getTransactionHistories(): Promise<TransactionHistory[]> {
     const response = await this.appService.getTransactionHistories();
     return response;
   }
---
+++ server/src/interfaces/app-transaction-controller.interface.ts
@@ -0,0 +1,13 @@
+import { DepositDTO } from 'src/dtos/deposit.dto';
+import { WithdrawDTO } from 'src/dtos/withdraw.dto';
+import { TransactionHistory } from 'src/schemas/transaction-history.schema';
+import { User } from 'src/schemas/user.schema';
+import { DepositResponse } from 'src/types/deposit-response.type';
+import { WithdrawResponse } from 'src/types/withdraw-response.type';
+
+export interface AppTransactionController {
+  createDeposit(depositDTO: DepositDTO): Promise<DepositResponse>;
+  createWithdraw(withdrawDTO: WithdrawDTO): Promise<WithdrawResponse>;
+  getBalance(): Promise<User>;
+  getTransactionHistories(): Promise<TransactionHistory[]>;
+}
